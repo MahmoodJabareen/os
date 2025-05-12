d14422bc206e714b22b7cc4731289c182dc954f1
MahmoodJabareen <11mahja11@gmail.com>
2025-05-12 10:34:18 +0000

    implementing lib trnmnt

--- ass2/user/libtournament.c
+++ ass2/user/libtournament.c
@@ -2,7 +2,22 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int tournament_create(int processes) {}
+int tournament_create(int processes) {
+    if( processes !=1 &&processes !=2 && processes !=4 && processes !=8 && processes !=16 )
+        return -1 ; // invalid number of locks
+    
+    int trnmnt_id = -1 ; 
+    int lock_id = 0 ;
+    int pid ;
+
+    for(int i = 1 ; i < processes ; i++ ){
+        if(peterson_create() < 0) {
+            return -1 ; // if one failed stop 
+        }
+    }
+    
+
+}
 
 int tournament_acquire(void) {}
 

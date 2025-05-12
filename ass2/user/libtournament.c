e6864e499127f8a6702640ecfe304876a7791d88
MahmoodJabareen <11mahja11@gmail.com>
2025-05-12 10:18:11 +0000

    added tournament

---
+++ ass2/user/libtournament.c
@@ -0,0 +1,9 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int tournament_create(int processes) {}
+
+int tournament_acquire(void) {}
+
+int tournament_release(void){}

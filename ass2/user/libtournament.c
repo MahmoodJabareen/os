5dcbc912028a0d719e50accc70f3af1dbb076d9c
MahmoodJabareen <11mahja11@gmail.com>
2025-05-12 17:50:53 +0000

    implementing lib trnmnt

--- ass2/user/libtournament.c
+++ ass2/user/libtournament.c
@@ -3,36 +3,20 @@
 #include "user/user.h"
 #include <math.h>
 
+#define MAX_PROCESSES 16
+#define MAX_LOCK (MAX_PROCESSES-1)
+
+int tournament_id = -1 ;
+int num_levels = 0 ;
+int num_processes = 0 ;
+int* lock_ids = 0 ;
+
+static int is_power_of_two(int x) {
+    return x > 0 && (x & (x - 1)) == 0;
+}
+
 int tournament_create(int processes) {
-    if( processes !=1 &&processes !=2 && processes !=4 && processes !=8 && processes !=16 )
-        return -1 ; // invalid number of locks
-    
-    int trnmnt_id = -1 ; 
-    int lock_id = 0 ;
-    int curr_pid ;
-
-    for(int i = 1 ; i < processes ; i++ ){
-        if(peterson_create() < 0) {
-            return -1 ; // if one failed stop 
-        }
-    }
-    
-    for(int i = 0 ; i < processes ; i++){
-        curr_pid = fork() ; 
-        if(curr_pid < 0){ // fork failed 
-            return -1 ;
-        }
-        else if(curr_pid == 0) {
-            for(int level = 0 ; level < processes / 2 ; level++) {
-                lock_id = i / (int)pow(2 , level) ;
-                peterson_acquire(lock_id , i % 2) ;
-            }
-            exit(0) ;
-        }
-    }
-    trnmnt_id = getpid() ; // parent id
-    return trnmnt_id ;
-
+   
 }
 
 int tournament_acquire(void) {}

4af29f9b86b15a75a6db283d092315d184ac49a5
MahmoodJabareen <11mahja11@gmail.com>
2025-05-14 14:25:36 +0000

    update

--- ass2/user/libtournament.c
+++ ass2/user/libtournament.c
@@ -5,10 +5,10 @@
 #define MAX_PROCESSES 16
 #define MAX_LOCKS (MAX_PROCESSES-1)
 
-int num_levels = 0 ;
-int num_processes = 0 ;
-int trnmnt_idx = 0;
-int lock_ids[MAX_LOCKS] ; 
+static int num_levels ;
+static int num_processes ;
+static int trnmnt_idx;
+static int lock_ids[MAX_LOCKS] ; 
 
 static int is_power_of_two(int x) {
     return x > 0 && (x & (x - 1)) == 0;
@@ -30,7 +30,7 @@ int tournament_create(int processes) {
    num_processes = processes ;
    num_levels = log2(processes) ;
 
-   for(int i = 0; i < processes -1 ; i++){
+   for(int i = 0; i < (processes -1) ; i++){
     lock_ids[i]= peterson_create() ;
     if(lock_ids[i] <0){
         return -1; //failed to create lock
@@ -44,7 +44,7 @@ int tournament_create(int processes) {
             return -1 ;
         if(pid == 0){
             trnmnt_idx = i ;
-            return trnmnt_idx ;
+            break ;
         }
    }
 return trnmnt_idx ;
@@ -54,13 +54,14 @@ int tournament_acquire(void){
 
     if(trnmnt_idx < 0 || num_levels <=0) return -1;
 
-    for(int lvl = 0 ; lvl < num_levels ; lvl++){
+    for(int lvl = (num_levels -1) ; lvl >= 0 ; lvl--){
         int role = (trnmnt_idx&(1<<(num_levels-lvl-1)))>>(num_levels-lvl-1) ;
         int lockl = trnmnt_idx>>(num_levels-lvl) ;
         int lockidx = lockl + (1<<lvl) -1 ;
         if(lockidx>=MAX_LOCKS) return -1 ;
 
-        peterson_acquire(lock_ids[lockidx] , role) ;
+        if(peterson_acquire(lock_ids[lockidx] , role) < 0)
+        return -1;
 
     }
 return 0 ;
@@ -70,18 +71,18 @@ int tournament_release(void) {
 
     if(trnmnt_idx < 0 || num_levels <=0) return -1;
 
-    for(int lvl = num_levels -1 ; lvl >=0  ; lvl--){
+    for(int lvl = 0 ; lvl < num_levels  ; lvl++){
         int role = (trnmnt_idx&(1<<(num_levels-lvl-1)))>>(num_levels-lvl-1) ;
         int lockl = trnmnt_idx>>(num_levels-lvl) ;
         int lockidx = lockl + (1<<lvl) -1 ;
         if(lockidx>=MAX_LOCKS) return -1 ;
 
-        peterson_release(lock_ids[lockidx] , role) ;
+        if (peterson_release(lock_ids[lockidx] , role) < 0)
+        return -1; 
 
     }
 return 0 ;
 
 
 }
-
 

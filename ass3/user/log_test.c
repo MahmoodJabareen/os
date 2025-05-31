51a4aa128cc3f12a03aad2cf302a835e4f84e029
MahmoodJabareen <11mahja11@gmail.com>
2025-05-31 22:27:11 +0000

    task2

--- ass3/user/log_test.c
+++ ass3/user/log_test.c
@@ -2,7 +2,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/riscv.h"
-
+#include <stdio.h>
 
 #define NCHILDREN 4
 
@@ -16,6 +16,47 @@ int main(){
         children_pids[i] = fork();
         if(children_pids[i] < 0)
             return -1 ;
+        
+        if(children_pids[i] == 0){//in child
+            int index = i ; 
+            sleep(100) ; //wait for parent to map (not the best solution )
+            char* shared_buffer = (char*)vas[index] ;
+
+            if(!shared_buffer){
+                printf("Child %d has no shared buffer..\n " , index) ;
+                exit(1) ;
+            }
+            //constructing the message
+            char msg[64] ;
+            int len = snprintf(msg , sizeof(msg) ,"Hello from child %d", index  );
+
+            uint64 addr  = (uint64) shared_buffer ;
+            uint64 end = addr + PGSIZE ;
+
+            //look for header 
+            while(addr + 4 + len < end){
+                uint32* header = (uint32*) addr ;
+                uint32 expected = 0 ;
+
+                uint32 new_header = (index << 16) | (len & 0xFFFF) ;
+
+
+                if(__sync_val_compare_and_swap(header , expected , new_header) == expected){
+
+                    //msg_ptr points to a shared memory 
+                    char* msg_ptr = (char*)(addr +4) ;
+                    for(int j = 0 ; j < len ; j++){
+                        msg_ptr[j] = msg[j] ;
+                    }
+                    break ;
+                }
+                 // skip to next aligned slot
+                addr += 4 + len;
+                addr = (addr + 3) & ~3; 
+
+            }
+            exit(0);
+        }
     }
 
     if(getpid() == parent_pid){ // in parent
@@ -27,6 +68,7 @@ int main(){
             }
         }
     }
+    
     
 
     return 0;

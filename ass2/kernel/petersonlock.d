ad42f112a56f328c817bd6142ae7530962e232b1
MahmoodJabareen <11mahja11@gmail.com>
2025-06-04 06:31:28 +0000

    final

--- ass2/kernel/petersonlock.d
+++
@@ -1,3 +0,0 @@
-kernel/petersonlock.o: kernel/petersonlock.c kernel/types.h \
- kernel/param.h kernel/memlayout.h kernel/spinlock.h kernel/riscv.h \
- kernel/proc.h kernel/defs.h kernel/petersonlock.h
